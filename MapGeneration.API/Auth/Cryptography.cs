ef7294fb29924bcd63c1517d88b064db386e6194
Maksim Ruksha <urpanium@gmail.com>
2024-11-27 21:56:14 +0300

    add cryptography class for password hashing

---
+++ MapGeneration.API/Auth/Cryptography.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MapGeneration.API.Auth;
+
+public static class Cryptography
+{
+    private const int Iterations = 10000;
+    private const int KeySize = 64;
+
+    public static string HashPassword(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(KeySize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(password),
+            salt,
+            Iterations,
+            HashAlgorithmName.SHA256,
+            KeySize
+        );
+        return Convert.ToHexString(hash);
+    }
+
+    public static bool VerifyPassword(string passwordHash, string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(KeySize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(password),
+            salt,
+            Iterations,
+            HashAlgorithmName.SHA256,
+            KeySize
+        );
+
+        return CryptographicOperations.FixedTimeEquals(hash, Convert.FromHexString(passwordHash));
+    }
+}
